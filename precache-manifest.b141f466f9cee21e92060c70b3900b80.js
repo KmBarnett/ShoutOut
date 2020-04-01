93fcffc1cb6cece87a6b900abff19753e7bf0a2b
kmbarnett <kmbarnett94@gmail.com>
2020-04-01 17:46:55 -0600

    Updates

---
+++ precache-manifest.b141f466f9cee21e92060c70b3900b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c8e576cb0c6fbd19a5cf46b14a6029",
+    "url": "/ShoutOut/index.html"
+  },
+  {
+    "revision": "d528e4db1289367da2f0",
+    "url": "/ShoutOut/static/css/main.f7f5c829.chunk.css"
+  },
+  {
+    "revision": "97af8ff51ca8e651ca28",
+    "url": "/ShoutOut/static/js/2.85b7a4d6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ShoutOut/static/js/2.85b7a4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d528e4db1289367da2f0",
+    "url": "/ShoutOut/static/js/main.ba2ad1de.chunk.js"
+  },
+  {
+    "revision": "76c19e0ec91274482a8e",
+    "url": "/ShoutOut/static/js/runtime-main.2f7264b6.js"
+  }
+]);
